8c994d66f531b123d0454cadc8acbf7bd030c76e
xyguo <xyguo.ubd@gmail.com>
2015-07-10 08:39:37 +0800

    first commit

---
+++ stanford/cs224d/assignment1/test_wordvec_sentiment.py
@@ -0,0 +1,41 @@
+__author__ = 'guoxy'
+
+import unittest
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+from wordvec_sentiment import *
+
+class MyTestCase(unittest.TestCase):
+
+    def test_softmax(self):
+        assert_array_almost_equal(softmax(np.array([[1001, 1002], [3, 4]])),
+                                  np.array([[0.26894142, 0.73105858],
+                                            [0.26894142, 0.73105858]]))
+        assert_array_almost_equal(softmax(np.array([[-1001, -1002]])),
+                                  [[0.73105858, 0.26894142]])
+        assert_array_almost_equal(softmax(np.array([1, 2])),
+                                  np.array([[0.26894142, 0.73105858]]))
+
+    def test_sigmoid_and_grad(self):
+        x = np.array([[1, 2], [-1, -2]])
+        f = sigmoid(x)
+        g = sigmoid_grad(f)
+        assert_array_almost_equal(f,
+                                  np.array([[0.731059, 0.880797],
+                                            [0.268941, 0.119203]]))
+        assert_array_almost_equal(g, np.array([[0.196612, 0.104994],
+                                               [0.196612, 0.104994]]))
+
+    def test_gradcheck_naive(self):
+        # Sanity check for the gradient checker
+        quad = lambda x: (np.sum(x ** 2), x * 2)
+
+        print "=== For autograder ==="
+        gradcheck_naive(quad, np.array(123.456))      # scalar test
+        gradcheck_naive(quad, np.random.randn(3,))    # 1-D test
+        gradcheck_naive(quad, np.random.randn(4,5))   # 2-D test
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ stanford/cs224d/assignment1/wordvec_sentiment.py
@@ -0,0 +1,118 @@
+__author__ = 'guoxy'
+import random
+import numpy as np
+from cs224d.data_utils import *
+import matplotlib.pyplot as plt
+
+# This is a bit of magic to make matplotlib figures appear inline in the notebook
+# rather than in a new window.
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+__all__ = ['softmax', 'sigmoid', 'sigmoid_grad', 'gradcheck_naive',
+           'forward_backward_prop']
+
+
+def softmax(x):
+    """ Softmax function """
+
+    e = np.exp(x.T - np.max(x.T, axis=0)).T
+    prob = (e.T / np.sum(e.T, axis=0)).T
+
+    if len(prob.shape) < 2:
+        prob = prob.reshape(1, prob.shape[0])
+    return prob
+
+
+def sigmoid(x):
+    """ Sigmoid function """
+
+    y = np.clip(x, a_min=-15, a_max=15)
+    y = 1 / (1 + np.exp(-y))
+
+    return y
+
+
+def sigmoid_grad(f):
+    """ Sigmoid gradient function """
+
+    g = f * (1 - f)
+
+    return g
+
+
+def gradcheck_naive(f, x):
+    """
+    Gradient check for a function f
+    - f should be a function that takes a single argument and outputs the cost and its gradients
+    - x is the point (numpy array) to check the gradient at
+    """
+
+    rndstate = random.getstate()
+    random.setstate(rndstate)
+    fx, grad = f(x) # Evaluate function value at original point
+    h = 1e-4
+
+    # Iterate over all indexes in x
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        ix = it.multi_index
+
+        ### YOUR CODE HERE: try modifying x[ix] with h defined above to compute numerical gradients
+        ### make sure you call random.setstate(rndstate) before calling f(x) each time, this will make it
+        ### possible to test cost functions with built in randomness later
+
+        return # replace this line with your code
+
+        ### END YOUR CODE
+
+        # Compare gradients
+        reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
+        if reldiff > 1e-5:
+            print "Gradient check failed."
+            print "First gradient error found at index %s" % str(ix)
+            print "Your gradient: %f \t Numerical gradient: %f" % (grad[ix], numgrad)
+            return
+
+        it.iternext() # Step to next dimension
+
+    print "Gradient check passed!"
+
+
+def forward_backward_prop(data, labels, params):
+    """ Forward and backward propagation for a two-layer sigmoidal network """
+    ###################################################################
+    # Compute the forward propagation and for the cross entropy cost, #
+    # and backward propagation for the gradients for all parameters.  #
+    ###################################################################
+
+    ### Unpack network parameters (do not modify)
+    t = 0
+    W1 = np.reshape(params[t:t+dimensions[0]*dimensions[1]], (dimensions[0], dimensions[1]))
+    t += dimensions[0]*dimensions[1]
+    b1 = np.reshape(params[t:t+dimensions[1]], (1, dimensions[1]))
+    t += dimensions[1]
+    W2 = np.reshape(params[t:t+dimensions[1]*dimensions[2]], (dimensions[1], dimensions[2]))
+    t += dimensions[1]*dimensions[2]
+    b2 = np.reshape(params[t:t+dimensions[2]], (1, dimensions[2]))
+
+    ### YOUR CODE HERE: forward propagation
+
+    # cost = ...
+
+    ### END YOUR CODE
+
+    ### YOUR CODE HERE: backward propagation
+
+    #gradW1 = ...
+    #gradb1 = ...
+    #gradW2 = ...
+    #gradb2 = ...
+
+    ### END YOUR CODE
+
+    ### Stack gradients (do not modify)
+    grad = np.concatenate((gradW1.flatten(), gradb1.flatten(), gradW2.flatten(), gradb2.flatten()))
+
+    return cost, grad
