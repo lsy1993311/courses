da96c6efc65e08d8e39214583e86a57141850641
xyguo <xyguo.ubd@gmail.com>
2015-07-13 09:19:58 +0800

    this is fucking life

---
+++ lamda/random_forest/test_forest.py
@@ -0,0 +1,14 @@
+__author__ = 'guoxy'
+
+import unittest
+import numpy as np
+from forest import *
+
+
+class ForestTestCase(unittest.TestCase):
+    def test_TreeNode(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
